151d8aa8e231e59094d5e0408bca7b0cd097cd75
Deeklming <deeklming6850@gmail.com>
2023-10-14 22:33:37 +0900

    test(fastapi): basic router test

--- yaxsons_fastapi/main.py
+++ yaxsons_fastapi/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from todo import todo_router
 
 app = FastAPI()
-
 
 @app.get("/")
 async def hi() -> dict:
     return {
-        "message": "Hi hi~2"
+        "message": "Hi2"
     }
+
+app.include_router(todo_router)
