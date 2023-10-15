e0e754160fa2adb2fb82a68feae226e77c4166b4
Deeklming <deeklming6850@gmail.com>
2023-10-15 17:41:01 +0900

    test(fastapi): other test

--- yaxsons_fastapi/model.py
+++ yaxsons_fastapi/model.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List
 
 class Item(BaseModel):
     item: str
@@ -7,3 +8,46 @@ class Item(BaseModel):
 class Todo(BaseModel):
     id: int
     item: Item
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "id": 10,
+                "item": {
+                    "item": "Nested models10",
+                    "status": "completed10"
+                }
+            }
+        }
+
+class TodoItem(BaseModel):
+    item: Item
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "item": {
+                    "item": "Nested models update",
+                    "status": "completed update"
+                }
+            }
+        }
+
+class TodoItems(BaseModel):
+    todos: List[TodoItem]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "todos":[
+                    {
+                        "item": "Nested models 21",
+                        "status": "completed 21"
+                    },
+                    {
+                        "item": "Nested models 22",
+                        "status": "completed 22"
+                    }
+                ]
+            }
+        }
