b7f0d46c5f4634bfdf84b498d4c8ae89e02304b5
Deeklming <deeklming6850@gmail.com>
2023-10-18 21:27:30 +0900

    feat(main): first step

--- yaxsons_fastapi/main.py
+++ yaxsons_fastapi/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
-from todo import todo_router
+
+import uvicorn
 
 app = FastAPI()
 
+# app.include_router(_router)
+
+# @app.on_event("startup")
+# def on_startup():
+#     connect_db()
+
 @app.get("/")
-async def hi() -> dict:
+async def home() -> dict:
     return {
-        "message": "Hi2"
+        "message": "home"
     }
 
-app.include_router(todo_router)
+if __name__=="__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
